9962352984dc4e3ee7b6602577af53ea4e882ac8
Juan Carlos Gutierrez <jc.gutierrezs@gmail.com>
2022-07-29 20:38:38 -0400

    relacion entre modelos, creacion fk

--- db/migrate/20181231205454_create_pet_histories.rb
+++ db/migrate/20181231205454_create_pet_histories.rb
@@ -2,7 +2,7 @@ class CreatePetHistories < ActiveRecord::Migration[5.2]
   def change
     create_table :pet_histories do |t|
       t.float :weight
-      t.string :heigth
+      t.float :heigth
       t.text :description
 
       t.timestamps
---
+++ db/migrate/20220729233943_add_ref_to_pet_history.rb
@@ -0,0 +1,5 @@
+class AddRefToPetHistory < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :pet_histories, :pet, foreign_key: true
+  end
+end
